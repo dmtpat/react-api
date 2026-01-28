fafacbbf134e898bf3add5394eb383939ec6b9fe
Pat <dmtpatricica@email.com>
2026-01-28 18:45:56 +0100

    actresses and actors separated list printing

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,36 @@ function App() {
   const [actors, setActors] = useState(0);
   const listaAttori = "https://lanciweb.github.io/demo/api/actors/";
   const listaAttrici = "https://lanciweb.github.io/demo/api/actresses/";
+  const [lists, setLists] = useState(listaAttori);
 
-  function getData() {
-    axios.get(listaAttori).then((results) => {
+  function getData(_list) {
+    axios.get(_list).then((results) => {
+      console.log("I dati sono stati richiesti");
       setActors(results.data)
-    }
-    )
+      console.log(actors);
+    }).catch(error => console.log(error.message))
   }
 
-  useEffect(getData, []);
+  useEffect(() => (getData(lists)), [lists]);
   return (
     <>
       <div className="boxed ">
         <h1>Hello React API</h1>
+        <button onClick={() => (setLists(listaAttori))}>Carica Attori</button>
+        <button onClick={() => (setLists(listaAttrici))}>Carica Attrici</button>
         <div className="card_container">
-          {actors.map((actor) => (
-            <div className='card' key={actor.id}>
-              <h2>{actor.name}</h2>
-              <img src={actor.image} alt="" />
-              <p>{actor.birth_year}</p>
-              <p>{actor.nationality}</p>
-              <span>{actor.biography}</span>
-              <h5>{actor.awards}</h5>
-
-            </div>
-          ))}
+          {actors.map((actor) => {
+            return (
+              <div className='card' key={actor.id}>
+                <h2>{actor.name}</h2>
+                <img src={actor.image} alt="" />
+                <p>{actor.birth_year}</p>
+                <p>{actor.nationality}</p>
+                <span>{actor.biography}</span>
+                <h5>{actor.awards}</h5>
+              </div>
+            )
+          })}
         </div>
       </div>
     </>
