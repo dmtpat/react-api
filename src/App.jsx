b9d86ead36e936396bf103499862be74cc27214a
Pat <dmtpatricica@email.com>
2026-01-28 17:17:49 +0100

    get data + printed in screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [actors, setActors] = useState(0);
+  const listaAttori = "https://lanciweb.github.io/demo/api/actors/";
+  const listaAttrici = "https://lanciweb.github.io/demo/api/actresses/";
 
+  function getData() {
+    axios.get(listaAttori).then((results) => {
+      setActors(results.data)
+    }
+    )
+  }
+
+  useEffect(getData, []);
   return (
     <>
       <h1>Hello React API</h1>
+      <ul>
+        {actors.map((actor) => (
+          <li key={actor.id}><h1>{actor.name}</h1></li>
+        ))}
+      </ul>
     </>
   )
 }
